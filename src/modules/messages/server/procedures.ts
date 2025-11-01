96c514bb236a08b129c4fac530291a0778cb1f56
Stephan Franz <stephan@worldanvil.com>
2025-11-01 12:47:39 -0500

    fix: implement billing and usage featurs

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -1,5 +1,6 @@
 import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
+import { consumeCredits } from "@/lib/usage";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -46,6 +47,23 @@ export const messagesRouter = createTRPCRouter({
           code: "NOT_FOUND",
           message: "Project not found",
         })
+      }
+
+      try {
+
+        await consumeCredits();
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Something went wrong. Please try again.",
+          })
+        } else {
+          throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "You have run out of credits. Please upgrade to continue.",
+          })
+        }
       }
 
       const createdMessage = await prisma.message.create({
